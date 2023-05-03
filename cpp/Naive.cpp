45d5a4c8f370433ca26453ac72c34b85c2e89b7a
Luka Govedic <luka.govedic@gmail.com>
2023-05-03 02:07:17 -0400

    Naive: refactored FFTs into different helpers

--- cpp/Naive.cpp
+++ cpp/Naive.cpp
@@ -20,11 +20,8 @@ namespace {
 }
 
 namespace ahr {
-    Naive::Naive(std::ostream &out, Dim M, Dim X, Dim Y) : HermiteRunner(out), M(M), X(X), Y(Y),
-                                                           momentsPH(M, X, Y), moments(M, X, Y), momentsPH_X(M, X, Y),
-                                                           momentsPH_Y(M, X, Y), moments_X(M, X, Y), moments_Y(M, X, Y),
-                                                           phiPH(X, Y),
-                                                           temp{forward_to_array<fftw::mdbuffer<2u>, 3u>(X, Y)} {}
+    Naive::Naive(std::ostream &out, Dim M, Dim X, Dim Y) : HermiteRunner(out), M(M), X(X), Y(Y), temp{
+            forward_to_array<fftw::mdbuffer<2u>, N_TEMP_BUFFERS>(X, Y)} {}
 
     void Naive::init(mdspan<Real, dextents<Dim, 3u>> initialMoments, Dim N_, Real initialDT_) {
         initialDT = initialDT_;
@@ -36,18 +33,18 @@ namespace ahr {
         // Currently assuming X==Y for simplicity, but the code is written generally for the most part.
         assert(X == Y);
 
+        // Plan FFTs both ways
+        fft = fftw::plan<2u>::dft(sliceXY(momentsReal, 0), sliceXY(moments.PH, 0), fftw::FORWARD, fftw::MEASURE);
+        fftInv = fftw::plan<2u>::dft(sliceXY(moments.PH, 0), sliceXY(momentsReal, 0), fftw::BACKWARD, fftw::MEASURE);
+
         // Initialize moments
         for_each_mxy([&](Dim m, Dim x, Dim y) {
-            moments(m, x, y) = initialMoments(m, x, y);
+            momentsReal(m, x, y) = initialMoments(m, x, y);
         });
-
-        // Plan FFTs both ways
-        plan = fftw::plan<2u>::dft(sliceXY(moments, 0), sliceXY(momentsPH, 0), fftw::FORWARD, fftw::MEASURE);
-        planInv = fftw::plan<2u>::dft(sliceXY(momentsPH, 0), sliceXY(moments, 0), fftw::BACKWARD, fftw::MEASURE);
 
         // Transform moments into phase space
         for (int m = 0; m < M; ++m) {
-            plan(sliceXY(moments, m), sliceXY(momentsPH, m));
+            fft(sliceXY(momentsReal, m), sliceXY(moments.PH, m));
         }
     }
 
@@ -56,13 +53,35 @@ namespace ahr {
         for (int t = 0; t < N; ++t) {
             // predictor step
 
+            // Phi
+            // TODO maybe compute PHI? Or maybe that happened in the previous timestep
+            prepareDXY_PH(phi.PH, phi.PH_DX, phi.PH_DY);
+            fftInv(phi.PH_DX, phi.DX);
+            fftInv(phi.PH_DY, phi.DY);
+
+            // Nabla
+            for_each_xy([&](Dim kx, Dim ky) {
+                auto dkx = double(kx), dky = double(ky), kPerp = dkx * dkx + dky * dky;
+                // TODO minus or not? (kperp should already have a minus)
+                nablaPerpAPar.PH_DX(kx, ky) = -dkx * 1i * kPerp * nablaPerpAPar.PH(kx, ky);
+                nablaPerpAPar.PH_DY(kx, ky) = -dky * 1i * kPerp * nablaPerpAPar.PH(kx, ky);
+            });
+            fftInv(nablaPerpAPar.PH_DX, nablaPerpAPar.DX);
+            fftInv(nablaPerpAPar.PH_DY, nablaPerpAPar.DY);
 
             for (int m = 2; m < M; ++m) {
-                bracketHalf(
-                        sliceXY(momentsPH, m), sliceXY(momentsPH_X, m), sliceXY(momentsPH_Y, m), sliceXY(moments_X, m),
-                        sliceXY(moments_Y, m), /*other*/ sliceXY(moments_X, A_PAR), sliceXY(moments_Y, A_PAR),
-                        temp[0], temp[1]);
+                ViewXY viewPH_X = sliceXY(moments.PH_DX, m), viewPH_Y = sliceXY(moments.PH_DY, m);
+                ViewXY viewX = sliceXY(moments.DX, m), viewY = sliceXY(moments.DY, m);
 
+                prepareDXY_PH(sliceXY(moments.PH, m), viewPH_X, viewPH.DY);
+                fftInv(viewPH_X, viewX);
+                fftInv(viewPH_Y, viewY);
+
+                ViewXY otherX = sliceXY(moments.DX, A_PAR),
+                        otherY = sliceXY(moments.DY, A_PAR);
+                bracket(viewX, viewY, otherX, otherY, temp[0]);
+
+                fft(temp[0], temp[1]);
             }
 
             // corrector step
@@ -74,12 +93,12 @@ namespace ahr {
 
     mdarray<Real, dextents<Dim, 3u>> Naive::getFinalValues() {
         for (int m = 0; m < M; ++m) {
-            planInv(sliceXY(momentsPH, m), sliceXY(moments, m));
+            fftInv(sliceXY(moments.PH, m), sliceXY(momentsReal, m));
         }
 
         mdarray<Real, dextents<Dim, 3u>> result{M, X, Y};
         for_each_mxy([&](Dim m, Dim x, Dim y) {
-            result(m, x, y) = moments(m, x, y).real();
+            result(m, x, y) = momentsReal(m, x, y).real();
         });
 
         return result;
--- cpp/Naive.h
+++ cpp/Naive.h
@@ -27,8 +27,26 @@ namespace ahr {
         using ViewXY = stdex::mdspan<Complex, stdex::dextents<Dim, 2u>>;
     private:
         Dim M, X, Y, N{};
-        fftw::plan<2u> plan{}, planInv{};
+        fftw::plan<2u> fft{}, fftInv{};
         Real initialDT{};
+
+        static constexpr Dim A_PAR = 1;
+        static constexpr Dim N_TEMP_BUFFERS = 3;
+
+        template<class Buffer>
+        struct BracketBuf {
+            using buffer_t = Buffer;
+            Buffer PH, PH_DX, PH_DY, DX, DY;
+
+            template<typename ...Args>
+            requires std::constructible_from<Buffer, Args...>
+            explicit BracketBuf(Args &&...args) :
+                    PH{std::forward<Args>(args)...},
+                    PH_DX{std::forward<Args>(args)...},
+                    PH_DY{std::forward<Args>(args)...},
+                    DX{std::forward<Args>(args)...},
+                    DY{std::forward<Args>(args)...} {}
+        };
 
         /// \defgroup Buffers for all the physical quantities used.
         /// Names ending in PH mean the values are in phase space.
@@ -37,22 +55,23 @@ namespace ahr {
         /// g_m: moment values for moments $m \in [0,M-1]$.
         /// The following values are special moments:
         /// - m=0: n_e (charge density)
-        /// - m=1: A|| (or Apar, parallel velocity)
-        ///
+        /// - m=1: A∥ (or Apar, parallel velocity)
         /// Additionally, moments is used only at the start and end.
         /// During the simulation, we use moments
         /// TODO maybe instead of these enormous amounts of memory, we could reuse (parallelism might suffer)
-        fftw::mdbuffer<3u> moments, momentsPH, momentsPH_X, momentsPH_Y, moments_X, moments_Y;
+        fftw::mdbuffer<3u> momentsReal{M, X, Y};
+        BracketBuf<fftw::mdbuffer<3u>> moments{M, X, Y};
 
-        /// \phi: the electrostatic potential.
-        fftw::mdbuffer<2u> phiPH;
+        /// Φ: the electrostatic potential.
+        BracketBuf<fftw::mdbuffer<2u>> phi{X, Y};
+
+        /// sq(∇⊥) A∥
+        BracketBuf<fftw::mdbuffer<2u>> nablaPerpAPar{X, Y};
 
         /// Temporary buffers used for various things.
-        std::array<fftw::mdbuffer<2u>, 3> temp;
+        std::array<fftw::mdbuffer<2u>, N_TEMP_BUFFERS> temp;
 
         /// @}
-
-        static constexpr Dim A_PAR = 1;
 
         void for_each_xy(std::invocable<Dim, Dim> auto fun) {
             for (Dim x = 0; x < X; ++x) {
@@ -78,25 +97,20 @@ namespace ahr {
         }
 
 
-        /// Computes bracket [viewPH, otherPH] assuming other has already been transformed into otherX and otherY.
-        /// Uses other views for temporary storage, and stores results in viewPH_X.
-        /// Currently really greedy.
-        void bracketHalf(const ViewXY &viewPH, ViewXY viewPH_X, ViewXY viewPH_Y, ViewXY view_X, ViewXY view_Y,
-                         const ViewXY &otherX,
-                         const ViewXY &otherY, ViewXY bracket, ViewXY bracketPH) {
+        /// Prepares the δx and δy of viewPH in phase space
+        void prepareDXY_PH(ViewXY viewPH, ViewXY viewPH_X, ViewXY viewPH_Y) {
             for_each_xy([&](Dim kx, Dim ky) {
                 viewPH_X(kx, ky) = -double(kx) * 1i * viewPH(kx, ky);
                 viewPH_Y(kx, ky) = -double(ky) * 1i * viewPH(kx, ky);
             });
+        }
 
-            planInv(viewPH_X, view_X);
-            planInv(viewPH_Y, view_Y);
-
+        /// computes bracket [view, other]
+        void bracket(const ViewXY &viewX, const ViewXY &view_Y, const ViewXY &otherX, const ViewXY &otherY,
+                     const ViewXY &bracket) {
             for_each_xy([&](Dim x, Dim y) {
-                bracket(x, y) = view_X(x, y) * otherY(x, y) - view_Y(x, y) * otherX(x, y);
+                bracket(x, y) = viewX(x, y) * otherY(x, y) - view_Y(x, y) * otherX(x, y);
             });
-
-            plan(bracket, bracketPH);
         }
     };
 
