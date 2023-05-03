6c27f665cf5645aa166bea0546528fc2d4b53b7c
rukalucas <79602580+rukalucas@users.noreply.github.com>
2023-05-03 16:54:45 -0400

    Correct exp_ng, indices in functions.jl

--- viriato_julia/functions.jl
+++ viriato_julia/functions.jl
@@ -3,13 +3,13 @@ include("grids.jl") #assuming this will exist
 
 # Need to resolve kperp array from grids
 
-function exp_nu(j, i, niu2, dti)
-    exp(-( niu*kperp[j,i]^2 + niu2*kperp[j,i]^(2*hyper_order))*dti)
+function exp_nu(i, j, niu2, dti)
+    exp(-( niu*kperp[i, j]^2 + niu2*kperp[i, j]^(2*hyper_order))*dti)
 end
 
-function exp_ng(dti)
-    exp(-(res*kperp[j,i]^2+res2*kperp[j,i]^(2*hyper_order))*dti/(1.0+kperp[j,i]^2*de^2))
+function exp_ng(ng,hyper_nuei,dti)
+    exp(-(ng*nu_ei+ng^(2*hyper_morder)*hyper_nuei)*dti)
 end
 
-function exp_eta(res2,dti)
-    exp(-(res*kperp[j,i]^2+res2*kperp[j,i]^(2*hyper_order))*dti/(1.0+kperp[j,i]^2*de^2))
+function exp_eta(i, j, res2,dti)
+    exp(-(res*kperp[i, j]^2+res2*kperp[i, j]^(2*hyper_order))*dti/(1.0+kperp[i, j]^2*de^2))
