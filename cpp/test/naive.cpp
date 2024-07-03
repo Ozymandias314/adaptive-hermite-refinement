724426a9940ce1aea90001f1354cc52de0e9a355
Luka Govedic <luka.govedic@gmail.com>
2024-07-03 18:49:23 -0400

    Added Gauss tests

--- cpp/test/naive.cpp
+++ cpp/test/naive.cpp
@@ -32,6 +32,40 @@ TEST(BasicNaive, OT01NoDiffusionEnergy) {
     EXPECT_THAT(mag_final + kin_final, LeTolerant(mag_init + kin_init, 1e-7));
 
     EXPECT_THAT(mag_final, AllClose(mag_init, 1e-5));
-    EXPECT_THAT(kin_final, AllClose(mag_final, 1e-5));
+    EXPECT_THAT(kin_final, AllClose(kin_init, 1e-5));
+    EXPECT_THAT(mag_final + kin_final, AllClose(mag_init + kin_init, 1e-5));
+}
+
+TEST(BasicNaive, GaussEnergy) {
+    ahr::Naive naive{std::cout, 10, 16, 16};
+    naive.init("gauss");
+    auto [mag_init, kin_init] = naive.calculateEnergies();
+
+    naive.run(10, 0); // no saving
+    auto [mag_final, kin_final] = naive.calculateEnergies();
+    EXPECT_THAT(mag_final, LeTolerant(mag_init, 1e-7));
+    EXPECT_THAT(kin_final, LeTolerant(kin_init, 1e-7, 1e-6)); // is zero, needs absolute tolerance
+    EXPECT_THAT(mag_final + kin_final, LeTolerant(mag_init + kin_init, 1e-7));
+
+    // TODO find a good lower bound for energies here
+}
+
+TEST(BasicNaive, GaussNoDiffusionEnergy) {
+    // turn off diffusion
+    ahr::nu = 0;
+    ahr::res = 0;
+
+    ahr::Naive naive{std::cout, 10, 16, 16};
+    naive.init("gauss");
+    auto [mag_init, kin_init] = naive.calculateEnergies();
+
+    naive.run(10, 0); // no saving
+    auto [mag_final, kin_final] = naive.calculateEnergies();
+    EXPECT_THAT(mag_final, LeTolerant(mag_init, 1e-7));
+    EXPECT_THAT(kin_final, LeTolerant(kin_init, 1e-7, 1e-6)); // is zero, needs absolute tolerance
+    EXPECT_THAT(mag_final + kin_final, LeTolerant(mag_init + kin_init, 1e-7));
+
+    EXPECT_THAT(mag_final, AllClose(mag_init, 1e-5));
+    EXPECT_THAT(kin_final, AllClose(kin_init, 1e-5, 1e-6)); // is zero, needs absolute tolerance
     EXPECT_THAT(mag_final + kin_final, AllClose(mag_init + kin_init, 1e-5));
 }
