531d09518e2941b953506f3d937abf22095b9781
Luka Govedic <luka.govedic@gmail.com>
2024-09-30 18:33:20 -0400

    Add naive benchmark

--- cpp/bench/naive.cpp
+++ cpp/bench/naive.cpp
@@ -1,5 +1,33 @@
+#include "Naive.h"
+
+#include <benchmark/benchmark.h>
 #include <iostream>
 
-int main() {
-  int
+using namespace ahr;
+static void BM_Naive(benchmark::State& state) {
+  std::ostringstream oss;
+
+  Dim const M = state.range(0);
+  Dim const X = state.range(1);
+  Dim const N = state.range(2);
+
+  Naive naive{oss, M, X, X};
+  naive.init("gauss");
+
+  // LArger
+  CFLFrac = 0.8;
+
+  // Run once, to get the timestep right, and avoid repeats during benchmarking
+  naive.run(1, 0);
+
+  // Benchmarking
+  for (auto _ : state) {
+    naive.run(N, 0);
+  }
 }
+
+BENCHMARK(BM_Naive)
+    ->ArgsProduct({{2, 4, 10, 45}, {128, 256, 512}, {5}})
+    ->ArgsProduct({{2, 4}, {1024, 2048}, {5}})
+    ->ArgsProduct({{2}, {4096}, {5}})
+    ->Unit(benchmark::kMillisecond);
