c7176f96d3a2df5204dfb2db55cd8db63ac3ba66
avelberg <111075338+avelberg@users.noreply.github.com>
2024-09-30 17:31:23 -0400

    Alex/diagnostics2 (#11)
    * Fixed energy diagnostics for symmetric mode
    
    * Fixed calculateEnergies in cpp
    
    * Normalize energies (cpp), formatting
    
    ---------
    
    Co-authored-by: Luka Govedič <ProExpertProg@users.noreply.github.com>

--- viriato_julia/diagnostics.jl
+++ viriato_julia/diagnostics.jl
@@ -6,7 +6,17 @@ include("constants.jl")
 function energy_tot(akpar,phik)
     b_energy_tot = 0.0
     phine_energy_tot = 0.0
-    for i = 1:nkx
+    
+    for j=1:nky
+        b_energy_tot += 0.5*kperp(1,j)^2*akpar[1,j]*conj(akpar[1,j])
+        if rhoi < small_rhoi
+            phine_energy_tot += 1.0*kperp(1,j)^2*phik[1,j]*conj(phik[1,j])
+        else
+            phine_energy_tot -= 1.0/rhoi^2*(Γ₀(kperp(1,j)^2*rhoi^2/2.0)-1)*phik[1,j]*conj(phik[1,j])
+        end
+    end
+    
+    for i = 2:nkx
         for j = 1:nky
             # Technically not correct, need to deal with double counting symmetric mode
             # Julia retains some complex part after this multiplication...numerical issue?
@@ -18,7 +28,7 @@ function energy_tot(akpar,phik)
             end
         end 
     end 
-    return b_energy_tot,phine_energy_tot
+    return b_energy_tot/nlx/nly,phine_energy_tot/nlx/nly
 end 
 
 #phine_energy(k)-1.0/rhoi**2*(gama0(kperp(j,1)**2*rhoi**2/2.)-1.)*&
